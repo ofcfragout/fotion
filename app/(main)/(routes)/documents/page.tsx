2a6233bab1980d2ab3d4c99e725790ffadfe4618
Ofc FragOut <gaby.puerto.rico@gmail.com>
2023-11-27 11:13:43 -0500

    Add files via upload
    init

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+import Image from 'next/image';
+import { useUser } from '@clerk/clerk-react'
+import { Button } from '@/components/ui/button';
+import { PlusCircle } from 'lucide-react';
+const DocumentsPage = () => {
+    const { user } = useUser();
+
+    return ( 
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+        <Image
+            src="/empty.png"
+            width="300"
+            height="300"
+            alt="Empty"
+            className="dark:hidden" />
+
+<Image
+            src="/empty-dark.png"
+            width="300"
+            height="300"
+            alt="Empty"
+            className="hidden dark:block" />
+
+            <h2 className='text-lg font-medium'>Welcome to {user?.firstName}&apos;s Fotion</h2>
+            <Button>
+                <PlusCircle className='h-4 w-4 mr-2' />
+                Create a Note
+            </Button>
+    </div> );
+}
+ 
+export default DocumentsPage;
